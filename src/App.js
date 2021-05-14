b0c5805053bfa808a079c5f02c77a071a985d17a
bejeju <bejejupark@gmail.com>
2021-05-15 00:33:29 +0900

    using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import LandingPage from './components/LandingPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hi (this is test)
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+    <Link to='/'/>
+    <Switch>
+      <Route exact path='/'><LandingPage /></Route>
+    </Switch>
+    </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function LandingPage() {
+    return (
+        <>
+            <div style={{ fontSize: '30px', display: 'flex', justifyContent: 'center' }}>
+                <p className='test'>프로젝트 100</p>
+            </div>
+            <br />
+            <hr />
+            <div style={{}}></div>
+        </>
+    )
+}
+
+export default LandingPage
